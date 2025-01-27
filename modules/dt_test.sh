bf092886622924195ad2e7fb054af41a63758966
ilawy <mm.ilawy@gmail.com>
2025-01-27 12:19:16 +0200

    write error message on invalid result

--- modules/dt_test.sh
+++ modules/dt_test.sh
@@ -2,10 +2,10 @@ source "./dt.sh"
 source "./common.sh"
 
 # VALID
-[[ $(dt_validate "string") == TRUE ]] && echo "(string) OK"
-[[ $(dt_validate "number") == TRUE ]] && echo "(number) OK"
-[[ $(dt_validate "boolean") == TRUE ]] && echo "(boolean) OK"
+[[ $(dt_validate "string") == TRUE ]] && echo "(string) OK" || echo "ERROR: (string)"
+[[ $(dt_validate "number") == TRUE ]] && echo "(number) OK" || echo "ERROR: (number)"
+[[ $(dt_validate "boolean") == TRUE ]] && echo "(boolean) OK" || echo "ERROR: (boolean)"
 echo "~~~ invalid cases ~~~"
 # INVALID
-[[ $(dt_validate " boolean") == FALSE ]] && echo "(non-trimmed) OK"
-[[ $(dt_validate "strng") == FALSE ]] && echo "(misspelled) OK"
+[[ $(dt_validate " boolean") == FALSE ]] && echo "(non-trimmed) OK" || echo "ERROR: (non-trimmed)"
+[[ $(dt_validate "strng") == FALSE ]] && echo "(misspelled) OK" || echo "ERROR: (misspelled)"
